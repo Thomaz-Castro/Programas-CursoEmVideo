b05e2f7e0221488cb7064ff559cd3d3bf94f36ee
Thomaz Castro <103527232+Thomaz-Castro@users.noreply.github.com>
2022-04-17 18:52:09 -0300

    Adicionei a cauculadora de IMC
    Coloquei o ex 043 (calculo de imc), dentro da pasta pra dar um push pro github

---
+++ Programas/ex 043 - Calculadora IMC.py
@@ -0,0 +1,23 @@
+#coleta os dados do usuário
+alt = float(input('Digite sua altura(m): '))
+pes = float(input('Digite seu peso(Kg): '))
+
+#calcula o IMC456
+imc = pes / (alt**2)
+
+#condicionais que mostrarão o resultado
+if imc < 18.5:
+    print('Seu IMC é {:.3f}'.format(imc))
+    print('Você está ABAIXO DO PESO')
+elif imc >= 18.5 and imc < 25:
+    print('Seu IMC é {:.3f}'.format(imc))
+    print('Você está no peso IDEAL')
+elif imc >= 25 and imc < 30:
+    print('Seu IMC é {:.3f}'.format(imc))
+    print('Você está com SOBREPESO')
+elif imc >= 30 and imc < 40:
+    print('Seu IMC é {:.3f}'.format(imc))
+    print('Você está com OBSIDADE')
+elif imc >= 40:
+    print('Seu IMC é {:.3f}'.format(imc))
+    print('Você está com OBSIDADE MÓRBIDA')
